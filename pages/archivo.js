d5bea6cb1ab0443190331daf0931eff4d4af6b4c
gisecalderon <gisecalderon08@gmail.com>
2022-11-09 19:06:28 -0300

    preentrega3

---
+++ pages/archivo.js
@@ -0,0 +1,87 @@
+let carrito = [];
+
+let btn_compra = document.querySelectorAll(".botonCompra");
+console.log(btn_compra);
+
+for(let boton of btn_compra){
+    boton.addEventListener("click" , agregar_a_carrito);
+}
+
+function agregar_a_carrito(e){
+
+    let hijo = e.target;
+    let padre = hijo.parentNode;
+    let abuelo = padre.parentNode;
+
+    console.log(hijo);
+    console.log(padre);
+    console.log(abuelo);
+
+    let nombre_producto = padre.querySelector("h5").textContent;
+    let precio_producto = padre.querySelector("span").textContent;
+    let imagen_producto = abuelo.querySelector("img").src;
+
+    console.log (nombre_producto);
+    console.log (precio_producto);
+    console.log(imagen_producto);
+
+    let producto = {
+        
+        nombre: nombre_producto,
+        precio: precio_producto,
+        imagen: imagen_producto,
+        cantidad: 1
+
+    }
+    
+    carrito.push(producto);
+    /*guardar en local storga formato json*/
+    
+    mostrar_carrito(producto);
+
+    }
+function mostrar_carrito (producto){
+    let fila = document.createElement("tr");
+    fila.innerHTML = `<td><img src="${producto.img}"></td>
+                        <td>${producto.nombre}</td>
+                        <td>${producto.cantidad}</td>
+                        <td>${producto.precio}</td>
+                        <td></td>
+                        <td><button class="btn btn-danger borrar_empanada">Borrar</td>`;
+        
+    console.log(fila);      
+    let tabla = document.getElementById("tbody");
+    tabla.append(fila);
+
+    let btn_borrar = document.querySelectorAll(".borrar_empanada");
+
+    for(let boton of btn_borrar){
+        boton.addEventListener("click" , borrar_producto);
+    }
+    }
+
+    function borrar_producto(e){
+
+        let abuelo = e.target.parentNode.parentNode;
+
+        abuelo.remove();
+    }
+
+    let btn_carrito = document.getElementById("mostrar_carrito");
+
+    btn_carrito.addEventListener("click", function(){
+
+        let carrito = document.getElementById("carrito");
+
+        if(carrito.style.display != "none"){
+
+            carrito.style.display = "none";
+        }
+        else{
+            carrito.style.display = "flex";
+        }
+
+    } )
+
+
+
